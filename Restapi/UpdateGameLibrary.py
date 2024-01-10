640813b461faf33a86bcdd748f5ebc868cb97df1
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2024-01-10 23:10:16 +0300

    Add files via upload

---
+++ Restapi/UpdateGameLibrary.py
@@ -0,0 +1,37 @@
+from steam import Steam
+from decouple import config
+import mysql.connector
+
+KEY = config("STEAM_API_KEY")
+ID =config("STEAM_ID")
+steam = Steam(KEY)
+
+games = steam.users.get_owned_games(ID)
+games = games['games']
+print(games[0]['appid'])
+
+
+
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="[PASSWORD]",
+  database="[DATABASE NAME]"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM auth_api_game")
+myresult = mycursor.fetchall()
+print(myresult)
+# db_game_id_list = []
+# for game in myresult:
+#     db_game_id_list.append(game[0])
+# print(db_game_id_list)
+# n = 0
+
+# for user_games in games:
+#     if user_games['appid'] in db_game_id_list:
+#         continue
+
+# mycursor.execute("INSERT INTO auth_api_game (game_name,app_id) VALUES('Raft',789)")
